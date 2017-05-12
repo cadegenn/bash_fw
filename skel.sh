495439b4bc7c18e10f8f993e65b43ae3b03075fb
DEGENNES <cadegenn@univ-lr.fr>
2017-05-12 16:02:11 +0200

    delete update() function and rearrange spaces on help screen

--- skel.sh
+++ skel.sh
@@ -69,29 +69,14 @@ usage() {
 	cat <<- EOF
 		DESCRIPTION: ${BASENAME} do some things
 		USAGE: ${BASENAME} [-d] [-dev] [-h] [-y]
-		 	-h	help screen (this screen)
-		 	-d	debug mode: print VARIABLE=value pairs
-		 	-dev	devel mode: print additional development data
-		 	-ask	ask for user confirmation for each individual eexec() call
-		 			NOTE: it is not affected by -y
-		 	-y	assume 'yes' to all questions
-		 	-s	simulate: do not really execute commands
-		 	--update	update libraries from github
+		    -h    help screen (this screen)
+		    -d    debug mode: print VARIABLE=value pairs
+		    -dev  devel mode: print additional development data
+		    -ask  ask for user confirmation for each individual eexec() call
+		          NOTE: it is not affected by -y
+		    -y    assume 'yes' to all questions
+		    -s    simulate: do not really execute commands
 	EOF
-	exit
-}
-
-# Update itself
-update() {
-	ebegin "Updating skel.sh"
-	eexec wget --no-check-certificate -q https://raw.githubusercontent.com/cadegenn/API/master/bash/skel.sh -O ${DIRNAME}/skel.sh
-	eend $?
-	ebegin "Updating libraries"
-	local FILES=$(find ${DIRNAME}/lib/ -mindepth 1 -type f -exec basename {} \;)
-	for F in ${FILES}; do
-		eexec wget --no-check-certificate -q https://raw.githubusercontent.com/cadegenn/API/master/bash/lib/${F} -O ${DIRNAME}/lib/${F}
-	done
-	eend $?
 	exit
 }
 
